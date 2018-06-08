d33baa83ee9f3b85e11175903de540e774ced562
Marco Favorito <marco.favorito@gmail.com>
2018-06-08 22:17:04 +0200

    Add wrapper for video recording

--- RLGames/gym_wrappers/GymPygameWrapper.py
+++ RLGames/gym_wrappers/GymPygameWrapper.py
@@ -1,9 +1,13 @@
+import os
 from abc import ABC
 from time import sleep
 
-from gym import Env
+import cv2
+from gym import Env, Wrapper
 from gym.utils import EzPickle
+from gym.wrappers import Monitor
 
+import pygame
 
 class GymPygameWrapper(ABC, Env, EzPickle):
 
@@ -12,7 +16,6 @@ class GymPygameWrapper(ABC, Env, EzPickle):
         EzPickle.__init__(self, *args, **kwargs)
 
         self.delay = delay
-
 
     @property
     def PygameEnvClass(self):
@@ -30,6 +33,7 @@ class GymPygameWrapper(ABC, Env, EzPickle):
         self.draw()
         sleep(self.delay)
 
+
     def getreward(self):
         r = self.current_reward
         return r
@@ -37,3 +41,39 @@ class GymPygameWrapper(ABC, Env, EzPickle):
     def reset(self):
         self.PygameEnvClass.reset(self)
         return self.getstate()
+
+
+class PygameVideoRecorder(Wrapper):
+    def __init__(self, env: GymPygameWrapper, directory):
+        super().__init__(env)
+
+        self.episode = -1
+
+        self.directory = directory
+        if not os.path.isdir(self.directory):
+            os.makedirs(self.directory, exist_ok=True)
+
+        self.shape = (self.env.win_width, self.env.win_height)
+        self.vid = None
+
+    def render(self, mode='human'):
+        self.env.render()
+
+        img = pygame.surfarray.array3d(self.env.screen)
+        img = img.swapaxes(1, 0)
+        # https://github.com/ContinuumIO/anaconda-issues/issues/223
+        # from RGB to BGR
+        img = img[:, :, ::-1].copy()
+        self.vid.write(img)
+        return img
+
+    def reset(self, *args, **kwargs):
+        self.episode += 1
+        if self.vid is not None:
+            self.vid.release()
+        self.vid = cv2.VideoWriter(self.directory + '/episode_{}.avi'.format(self.episode),
+                                   cv2.VideoWriter_fourcc(*"XVID"), 30, self.shape, 1)
+
+        return self.env.reset(*args, **kwargs)
+
+
